d7678c3db213b88d55b1763709a8530f1f56206c
Felix Olsson <felix.olsson1910@gmail.com>
2026-01-29 11:33:02 +0100

    enter knapp

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("Hej från webbläsaren!")
+const myBody = document.querySelector("body")
+
+
+myBody.addEventListener("keydown", function (event) {
+
+    if (event.key === "Enter") {
+        console.log("Du har klickat Enter")
+    }
+    else if (event.key === "Backspace") {
+        console.log("Du har klickat Backspace")
+    }
+    else {
+        console.log(event.key)
+    }
+    
+})
